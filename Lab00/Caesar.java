7e44d66054ad8f66e6784407f276faed56525a38
Canek García <kanect@rocketmail.com>
2018-08-30 19:32:46 -0500

    worikin on new version of Caesar Cipher

--- Lab00/Caesar.java
+++ Lab00/Caesar.java
@@ -1,32 +1,59 @@
+import java.io.BufferedReader; 
+import java.io.IOException; 
+import java.io.InputStreamReader;
+import java.lang.*;
+
 public class Caesar { 
 
-    public static void main(String[] arg){ 
+    static String alfabetoMinusculas = "abcdefghijklmnñopqrstuvwxyz"; 
+    static String alfabetoMayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
+    static BufferedReader bf         = new BufferedReader(new InputStreamReader(System.in));
 
-        String Cadena        = "Esta es una cadena de prueba"; 
-        String CadenaCifrada = ""; 
-        int Desplazamiento   = 3; 
-
-		System.out.println(Cadena);
-		
-        CadenaCifrada = cifrar(Cadena, Desplazamiento); 
-        System.out.println(CadenaCifrada); 
-    } 
-
-    private static String cifrar(String Cadena, int Desplazamiento){ 
-        String alfabetoMinusculas = "abcdefghijklmnñopqrstuvwxyz";
-        String alfabetoMayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
+    /**
+     * E(x) = x+n (mod 27)
+     */
+    private static String codificar(String cadena, int desplazamiento){
         String rtnCadena = ""; 
 
-        for(int i=0; i<Cadena.length(); i++){ 
-            if( (alfabetoMinusculas.indexOf(Cadena.charAt(i)) != -1) || (alfabetoMayusculas.indexOf(Cadena.charAt(i)) != -1) ) 
-                rtnCadena += (alfabetoMinusculas.indexOf(Cadena.charAt(i)) != -1) 
-                            ? alfabetoMinusculas.charAt(  ( alfabetoMinusculas.indexOf(Cadena.charAt(i)) +Desplazamiento)%alfabetoMinusculas.length() ) 
-                            : alfabetoMayusculas.charAt(  ( alfabetoMayusculas.indexOf(Cadena.charAt(i)) +Desplazamiento)%alfabetoMayusculas.length() ); 
-            else 
-                rtnCadena += Cadena.charAt(i); 
+        for(int i=0; i<cadena.length(); i++){ 
+
+            CharSequence caPattern = cadena.charAt(i);
+            
+            if( (alfabetoMinusculas.indexOf(cadena.charAt(i)) != -1) || 
+                (alfabetoMayusculas.indexOf(cadena.charAt(i)) != -1) ) {
+//https://www.geeksforgeeks.org/searching-for-character-and-substring-in-a-string/
+
+            }
+
+            /*
+            if( (alfabetoMinusculas.indexOf(cadena.charAt(i)) != -1) || 
+                (alfabetoMayusculas.indexOf(cadena.charAt(i)) != -1) ) {
+                // rtnCadena += (alfabetoMinusculas.indexOf(cadena.charAt(i)) != -1) 
+                //             ? alfabetoMinusculas.charAt(  ( alfabetoMinusculas.indexOf(cadena.charAt(i)) +Desplazamiento)%alfabetoMinusculas.length() ) 
+                //             : alfabetoMayusculas.charAt(  ( alfabetoMayusculas.indexOf(cadena.charAt(i)) +Desplazamiento)%alfabetoMayusculas.length() ); 
+            }                            
+            else{
+            //     rtnCadena += cadena.charAt(i); 
+            }
+            */
+
         } 
 
         return rtnCadena;
     }
+
+
+
+    public static void main(String[] arg){ 
+
+        String cadena           = "Esta es una cadena de prueba";
+        int desplazamiento      = 3;
+        String cadenaCodificada = ""; 
+
+		System.out.println("Texto original:   " + cadena);
+		
+        cadenaCodificada = codificar(cadena, desplazamiento); 
+        System.out.println("Texto codificado: " + cadenaCodificada); 
+    } 
 
 } 
