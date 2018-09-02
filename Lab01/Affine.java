d97234cdf81d7f20cde701d9bbd9f0a9ee2dd07c
Canek García <kanect@rocketmail.com>
2018-09-02 00:46:38 -0500

    updated method 'codificar' in Affine class

--- Lab01/Affine.java
+++ Lab01/Affine.java
@@ -1,39 +1,53 @@
 public class Affine { 
 
-   public static void main(String[] args) {
-       String cadena = "esta es una cadena de prueba";
-       int a = 5;
-       int b = 15;
+    static String alfabetoMinusculas = "abcdefghijklmnñopqrstuvwxyz"; 
+    static String alfabetoMayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
 
-       String cadenaCifrada = cifrar(cadena, a, b);
-       System.out.println("Texto claro:   " + cadena);
-       System.out.println("Texto cifrado: " + cadenaCifrada);
-   }
-   
+    /**
+     * E(x) = (ax + b) mod n
+     */
+    private static String codificar(String cadena, int a, int b) {
+        String rtnCadena = ""; 
 
-   /**
-    * c = (am + b) mod n
-    * @param  cadena [description]
-    * @param  a      [description]
-    * @param  b      [description]
-    * @return        [description]
-    */
-   public static String cifrar(String cadena, int a, int b) {
-       String str = "";
+        for (int i=0; i<cadena.length(); i++) {
 
-       for (int i=0; i<cadena.length(); i++) {
-           
-           char singleCharacter = cadena.charAt(i);
-           
-           if (Character.isLetter(singleCharacter)) { //filtro, solo letras
-               // (ax + b) % 27
-               singleCharacter = (char) ( (a * (int)(singleCharacter + 'a') + b) % 27 + 'a'); 
-           }
-           
-           str +=singleCharacter;
-       }
+            if( alfabetoMayusculas.contains( String.valueOf(cadena.charAt(i)) ) ){
+                int x  = alfabetoMayusculas.indexOf(cadena.charAt(i));
+                int Ex = ((a*x) + b) % alfabetoMayusculas.length();
+                char cipherChar = alfabetoMayusculas.charAt(Ex);
+                rtnCadena += cipherChar; 
+            }
+            // char singleCharacter = cadena.charAt(i);
+            // if (Character.isLetter(singleCharacter)) { //filtro, solo letras
+            //     // (ax + b) % 27
+            //     singleCharacter = (char) ( (a * (int)(singleCharacter + 'a') + b) % 27 + 'a'); 
+            // }
+            // str +=singleCharacter;
 
-       return str;
-   }
+        }
 
-} 
+        return rtnCadena;
+    }
+
+
+    /**
+     * 
+     */
+    public static void main(String[] args) {
+        String cadena = "TARINGA";
+        int a = 17;
+        int b = 5;
+
+        // 1. Texto original
+		System.out.println("Texto original:     " + cadena);
+		System.out.println();
+		// 2. Codificar
+        String cadenaCodificada = codificar(cadena, a, b); 
+        System.out.println("Texto codificado:   " + cadenaCodificada); 
+        // 3. Decodificar
+        // cadenaDecodificada = decodificar(cadenaCodificada, desplazamiento); 
+        // System.out.println("Texto decodificado: " + cadenaDecodificada); 
+
+    }
+
+}
