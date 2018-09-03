3dad9c48848cfdf93a28f512ff744f627d9c3381
Canek García <kanect@rocketmail.com>
2018-09-03 00:33:43 -0500

    added 'decodificar' method in Affine class

--- Lab01/Affine.java
+++ Lab01/Affine.java
@@ -1,3 +1,5 @@
+import java.math.*;
+
 public class Affine { 
 
     static String alfabetoMinusculas = "abcdefghijklmnñopqrstuvwxyz"; 
@@ -13,32 +15,63 @@ public class Affine {
 
             if( alfabetoMayusculas.contains( String.valueOf(cadena.charAt(i)) ) ){
                 int x  = alfabetoMayusculas.indexOf(cadena.charAt(i));
+                // E(x)= ( a x  + b)                 mod n
                 int Ex = ((a*x) + b) % alfabetoMayusculas.length();
                 char cipherChar = alfabetoMayusculas.charAt(Ex);
+
                 rtnCadena += cipherChar; 
             }else{
                 rtnCadena += cadena.charAt(i); 
             }
-            // char singleCharacter = cadena.charAt(i);
-            // if (Character.isLetter(singleCharacter)) { //filtro, solo letras
-            //     // (ax + b) % 27
-            //     singleCharacter = (char) ( (a * (int)(singleCharacter + 'a') + b) % 27 + 'a'); 
-            // }
-            // str +=singleCharacter;
-
+            
         }
 
         return rtnCadena;
     }
 
+    /**
+     * D(x) = ( a^-1 (x-b) ) mod n
+     */
+    private static String decodificar(String cadena, int a, int b){
+        String rtnCadena = ""; 
+
+        // 1. inverso multiplicativo
+        int a_inv = 0;
+        int flag = 0;
+        for (int i = 0; i < 27; i++){
+            flag = (a * i) % 27;
+            if (flag == 1)
+            {
+                a_inv = i;
+                // System.out.println(i);
+            }
+        }
+
+        // 2. aplicando formula de decifrado
+        for (int i=0; i<cadena.length(); i++) {
+
+            if( alfabetoMayusculas.contains( String.valueOf(cadena.charAt(i)) ) ){
+                int x  = alfabetoMayusculas.indexOf(cadena.charAt(i));
+                // int aInverse = Math.pow(a,-1);
+                int modN = alfabetoMayusculas.length();
+				int Ex = ( ((a_inv)*(x-b+modN))%modN ); //% alfabetoMayusculas.length();
+                char cipherChar = alfabetoMayusculas.charAt(Ex);
+                rtnCadena += cipherChar; 
+            }else{
+                rtnCadena += cadena.charAt(i); 
+            }
+        }
+
+        return rtnCadena;
+    }
 
     /**
-     * 
+     * Método main
      */
     public static void main(String[] args) {
-        String cadena = "ESTA ES UNA CADENA DE PRUEBA";
-        int a = 17;
-        int b = 5;
+        String cadena = "AFFINE CIPHER";
+        int a = 5;
+        int b = 8;
 
         // 1. Texto original
 		System.out.println("Texto original:     " + cadena);
@@ -47,8 +80,8 @@ public class Affine {
         String cadenaCodificada = codificar(cadena, a, b); 
         System.out.println("Texto codificado:   " + cadenaCodificada); 
         // 3. Decodificar
-        // cadenaDecodificada = decodificar(cadenaCodificada, desplazamiento); 
-        // System.out.println("Texto decodificado: " + cadenaDecodificada); 
+        String cadenaDecodificada = decodificar(cadenaCodificada, a, b); 
+        System.out.println("Texto decodificado: " + cadenaDecodificada); 
 
     }
 
