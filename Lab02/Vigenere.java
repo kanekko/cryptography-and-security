a6137cc1ad48a3b8ccc77f2b6b294f357716461c
Canek García <kanect@rocketmail.com>
2018-09-07 16:37:29 -0500

    working on new version of Vigenere cipher

--- Lab02/Vigenere.java
+++ Lab02/Vigenere.java
@@ -1,24 +1,46 @@
 public class Vigenere {
-	
-    public static void main(String[] args) {
-        String key = "HOLA";
-        String message = "ESTA ES LA PRUEBA";
-        String enc = encrypt(message, key);
 
-        System.out.println("Mensaje: " + message);
-		System.out.println("Llave: " + key);		
-        System.out.println("Vigenere: " + enc);
-    }
- 
-    static String encrypt(String text, final String key) {
-        String res = "";
-        text = text.toUpperCase();
-        for (int i = 0, j = 0; i < text.length(); i++) {
-            char c = text.charAt(i);
-            if (c < 'A' || c > 'Z') continue;
-            res += (char)((c + key.charAt(j) + 'A') % 27 + 'A');
+    static String alfabetoMayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
+
+    /**
+     * E(x) = (Xi + Ki) mod L
+     */
+    private static String codificar(String cadena, String llave) {
+        String rtnCadena = "";
+
+        for (int i=0; i<cadena.length(); i++) {
+            if( alfabetoMayusculas.contains( String.valueOf(cadena.charAt(i)) ) ){
+                int x  = alfabetoMayusculas.indexOf(cadena.charAt(i));
+                // E(x) = (Xi + Ki) mod L
+                int Ex = ( x + b) % alfabetoMayusculas.length();
+                // char cipherChar = alfabetoMayusculas.charAt(Ex);
+
+                // rtnCadena += cipherChar; 
+            }else{
+                rtnCadena += cadena.charAt(i); 
+            }
         }
-        return res;
+
+        return rtnCadena;
     }
- 
+
+    /**
+     * Método principal
+     */
+    public static void main(String[] args) {
+        String mensaje = "PER ASPERA AD ASTRA";
+        String llave   = "VIGENERE";
+
+        // 1. Texto original
+		System.out.println("Texto original:     " + mensaje);
+		System.out.println("Llave:              " + llave);
+		System.out.println();        
+		// 2. Codificar
+        String mensajeCodificado = codificar(mensaje, llave); 
+        System.out.println("Texto codificado:   " + mensajeCodificado); 
+        // 3. Decodificar
+        // String cadenaDecodificada = decodificar(cadenaCodificada, a, b); 
+        // System.out.println("Texto decodificado: " + cadenaDecodificada); 
+    }
+
 }
