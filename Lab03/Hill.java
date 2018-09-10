226738c3507d5a2c7add9c30ccffd9e0ebf3f928
Canek García <kanect@rocketmail.com>
2018-09-10 11:30:42 -0500

    base code to Hill cipher

---
+++ Lab03/Hill.java
@@ -0,0 +1,44 @@
+public class Hill {
+
+    static String alfabetoMayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
+
+    /**
+     * 
+     */
+    private static String codificar(String cadena) {
+        String rtnCadena = "";
+
+        return rtnCadena;
+    }
+
+    /**
+     * 
+     */
+    private static String decodificar(String cadena) {
+        String rtnCadena = "";
+
+
+        return rtnCadena;
+    }
+
+
+    /**
+     * Método principal
+     */
+    public static void main(String[] args) {
+        String mensaje = "PER ASPERA AD ASTRA";
+        String llave   = "VIGENERE";
+
+        // 1. Texto original
+		System.out.println("Texto original:     " + mensaje);
+		System.out.println("Llave:              " + llave);
+		System.out.println();        
+		// 2. Codificar
+        // String mensajeCodificado = codificar(mensaje, llave); 
+        // System.out.println("Texto codificado:   " + mensajeCodificado); 
+        // 3. Decodificar
+        // String cadenaDecodificada = decodificar(mensajeCodificado, llave); 
+        // System.out.println("Texto decodificado: " + cadenaDecodificada); 
+    }
+
+}
