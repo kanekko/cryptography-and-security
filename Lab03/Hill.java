a60edd3629847eabc7adebc59533def40680310b
Canek García <kanect@rocketmail.com>
2018-09-30 19:06:04 -0500

    added multiplicaMatrices and getMessageCipher methods

--- Lab03/Hill.java
+++ Lab03/Hill.java
@@ -1,5 +1,7 @@
 import java.math.*;
 
+import jdk.nashorn.internal.ir.ReturnNode;
+
 public class Hill {
 
     static String alfabetoMayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
@@ -11,15 +13,16 @@ public class Hill {
         String rtnCadena = "";
 
         //1. Construcción y verificación de la Matriz correspondiente a la clave
-        int[][] claveMatriz = getMatrizClave(clave);
+        int[][] matrizClave = getMatrizClave(clave);
 
         //2. Construcción de n-gramas del mensaje
-        int[][] mensajeMatriz = getMatrizMensaje(mensaje, claveMatriz[0].length);
+        int[][] matrizMensaje = getMatrizMensaje(mensaje, matrizClave.length);
 
         //3. Multiplicamos Matrices 
-        int [][] claveXmensaje = multiplicaMatrices(claveMatriz, mensajeMatriz);
+        int [][] claveXmensaje = multiplicaMatrices(matrizClave, matrizMensaje);
 
         //4. Recuperamos mensaje ofuscado
+        rtnCadena = getMessageCipher(claveXmensaje);
 
         return rtnCadena;
     }
@@ -55,27 +58,35 @@ public class Hill {
     }
 
     /**
-     * Se tiene que ver inversa esta matriz
+     * NOTA: este método regresa la matriz de manera transpuesta, es decir:
+     * [ 2  15 13 ]
+     * [ 19 21 11 ]
+     * en lugar de :
+     * [ 2 19] 
+     * [15 21] 
+     * [13 11] 
+     * debido a que es más fácil trabajar con la primer versión de matriz y 
+     * por el mismo diseño del lenguaje Java.
      * @param mensaje
      * @return
      */
-    private static int[][] getMatrizMensaje(String mensaje, int longitudeMatriz){
+    private static int[][] getMatrizMensaje(String mensaje, int longitudeMatrizClave){
         int[][] rtnMensaje = new int[0][0];
 
-        if( mensaje.length()%longitudeMatriz == 0 ){
-            int column = mensaje.length() / longitudeMatriz;
-            rtnMensaje = new int[column][longitudeMatriz];
+        if( mensaje.length()%longitudeMatrizClave == 0 ){
+            int rows = mensaje.length()/longitudeMatrizClave; //length of N-gramas
+            rtnMensaje = new int[rows][longitudeMatrizClave]; //initializing matrix
 
-            int flagMesaje = 0;
-            for(int i=0; i<column; i++){
-                for(int j=0; j<longitudeMatriz; j++){
-                    rtnMensaje[i][j] = alfabetoMayusculas.indexOf(mensaje.charAt(flagMesaje));
-                    flagMesaje++;
+            int flagPositionMesaje = 0;
+            for(int i=0; i<rows; i++){ // i-rows
+                for(int j=0; j<longitudeMatrizClave; j++){ // j-columns
+                    rtnMensaje[i][j] = alfabetoMayusculas.indexOf(mensaje.charAt(flagPositionMesaje));
+                    flagPositionMesaje++;
                 }
             }
 
         }else{
-            System.out.println("Mensaje invalido, no se puede formar una matrices de Nx1");
+            System.out.println("Mensaje invalido, no se puede formar matrices de Nx1");
         }
 
         return rtnMensaje;
@@ -83,24 +94,42 @@ public class Hill {
 
     /**
      * 
+     * @param matrizClave
+     * @param matrizMensaje
+     * @return Matriz con operaciónes resueltas y el modulo calculado en cada posición.
      */
-    private static int[][] multiplicaMatrices(int[][] clave, int[][] mensaje){
-        int[][] rtnMultiplicacion = new int[3][3];
-
-        // for(int i=0; i<mensaje.length; i++){
-
-            for(int x=0; x<clave[x].length; x++){
-                for(int y=0; y<clave[x].length; y++){
-                    // rtnMultiplicacion[x][y] = clave[x][y] * mensaje[i][y];
-                    System.out.print(clave[x][y]);
-                }
-            }
-
-        // }
+    private static int[][] multiplicaMatrices(int[][] matrizClave, int[][] matrizMensaje){
+        int[][] rtnMultiplicacion = new int[matrizMensaje.length][matrizMensaje[0].length];
         
+        for(int h=0; h<rtnMultiplicacion.length;h++){
+            for(int i=0; i<matrizClave.length; i++){ // looping on rows of matrizMensaje
+                int a = 0;
+                for(int j=0; j<matrizMensaje[0].length; j++){
+                    a = a + (matrizClave[i][j]*matrizMensaje[h][j]);
+                }
+                rtnMultiplicacion[h][i] = a % alfabetoMayusculas.length();
+            }
+        }
+
         return rtnMultiplicacion;
     }
 
+    /**
+     * 
+     * @param matrizChiper
+     * @return
+     */
+    private static String getMessageCipher(int[][] matrizChiper){
+        String rtnMensaje = "";
+
+        for(int i=0; i<matrizChiper.length; i++){
+            for(int j=0; j<matrizChiper[i].length; j++){
+                rtnMensaje = rtnMensaje + ( alfabetoMayusculas.charAt(matrizChiper[i][j]) + "");
+            }
+        }
+
+        return rtnMensaje;
+    }
     
 
     /**
