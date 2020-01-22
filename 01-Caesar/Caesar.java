a5e37e2213d9693f60baa06c31bba2887098c93d
Canek <kaan.ek@ciencias.unam.mx>
2020-01-21 18:35:11 -0600

    Working in new version of Caesar Cypher

--- 01-Caesar/Caesar.java
+++ 01-Caesar/Caesar.java
@@ -1,39 +1,42 @@
+/**
+ * 
+ */
 public class Caesar { 
 
     // Member varialbes
-    static String alfabetoMinusculas = "abcdefghijklmnñopqrstuvwxyz";
-    static String alfabetoMayusculas = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
+    static String alphabetMinus = "abcdefghijklmnñopqrstuvwxyz";
+    static String alphabetMayus = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"; 
 
     /**
      * E(x) = x+n (mod N)
      * 
-     * @param cadena         plain text
-     * @param desplazamiento total of displacements
-     * @return               string object with ciphertext
+     * @param cadena       plain text
+     * @param displacement total of displacements
+     * @return             string object with ciphertext
      */
-    private static String codificar(String cadena, int desplazamiento){
+    private static String encrypt(String plainText, int displacement){
         String rtnCadena = ""; 
 
-        for(int i=0; i<cadena.length(); i++){
+        for(int i=0; i<plainText.length(); i++){
 
-            if ( alfabetoMinusculas.contains( String.valueOf(cadena.charAt(i)) ) || // Filtering characters 
-                 alfabetoMayusculas.contains( String.valueOf(cadena.charAt(i)) ) ) {
+            if ( alfabetoMinusculas.contains( String.valueOf(plainText.charAt(i)) ) || // Filtering characters 
+                 alfabetoMayusculas.contains( String.valueOf(plainText.charAt(i)) ) ) {
                 
                 char cipherChar;
                 
-                if ( alfabetoMinusculas.contains( String.valueOf(cadena.charAt(i)) ) ) { // Minus case
+                if ( alfabetoMinusculas.contains( String.valueOf(plainText.charAt(i)) ) ) { // Minus case
                     // index of a char (in loop)
-                    int x  = alfabetoMinusculas.indexOf(cadena.charAt(i));
+                    int x  = alfabetoMinusculas.indexOf(plainText.charAt(i));
                     // E(x)=  x +        n                   (mod N)
-                    int Ex = (x + desplazamiento) % alfabetoMinusculas.length();
+                    int Ex = (x + displacement) % alfabetoMinusculas.length();
                     // getting index of chipher char
                     cipherChar = alfabetoMinusculas.charAt(Ex);
                 
                 }else{ // Mayus case
 
-                    int x  = alfabetoMayusculas.indexOf(cadena.charAt(i));
+                    int x  = alfabetoMayusculas.indexOf(plainText.charAt(i));
                     // E(x)=  x +        n             (mod N)
-                    int Ex = (x + desplazamiento) % alfabetoMayusculas.length();
+                    int Ex = (x + displacement) % alfabetoMayusculas.length();
                     
                     cipherChar = alfabetoMayusculas.charAt(Ex);
                 }
@@ -41,7 +44,7 @@ public class Caesar {
                 rtnCadena += cipherChar; // Union of all chars
 
             }else{
-                rtnCadena += cadena.charAt(i); 
+                rtnCadena += plainText.charAt(i); 
             }
 
         } 
@@ -56,7 +59,7 @@ public class Caesar {
      * @param desplazamiento total of displacements
      * @return               string object with plain text
      */
-    private static String decodificar(String cadena, int desplazamiento){
+    private static String decrypt(String cadena, int desplazamiento){
         String rtnCadena = ""; 
 
         for(int i=0; i<cadena.length(); i++){
@@ -99,24 +102,24 @@ public class Caesar {
      * 
      * @param args arguments
      */
-    public static void main(String[] args){ 
-        String cadena      = "Esta es una prueba";
-        int desplazamiento = 3;
+    public static void main(String[] args){
+        String plainText = "Esta es una prueba";
+        int displacement = 3;
 
-        String cadenaCodificada   = ""; 
-        String cadenaDecodificada = ""; 
+        String cryptogram    = "";
+        String decryptedText = "";
 
-        // 1. Texto original
-		System.out.println("Texto original:     " + cadena);
+        // 1. Plain text
+		System.out.println("Texto plano:     " + plainText);
 		System.out.println();
 
-		// 2. Codificar
-        cadenaCodificada = codificar(cadena, desplazamiento); 
-        System.out.println("Texto codificado:   " + cadenaCodificada); 
+		// 2. Encrypt
+        cryptogram = encrypt(plainText, displacement);
+        System.out.println("Texto codificado:   " + cryptogram);
 
-        // 3. Decodificar
-        cadenaDecodificada = decodificar(cadenaCodificada, desplazamiento); 
-        System.out.println("Texto decodificado: " + cadenaDecodificada); 
-    } 
+        // 3. Decrypt
+        decryptedText = decrypt(cryptogram, displacement);
+        System.out.println("Texto decodificado: " + decryptedText);
+    }
 
 } 
