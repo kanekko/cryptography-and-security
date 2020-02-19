e705f5f966d6dc00b6fab516d918b1a2f413c9df
Canek <kaan.ek@ciencias.unam.mx>
2020-02-19 12:47:28 -0600

    Fixed issue in Affine cipher

--- 02-Affine/Affine.java
+++ 02-Affine/Affine.java
@@ -101,7 +101,9 @@ public class Affine {
      * @param args arguments
      */
     public static void main(String[] args){
-        String plainText = "Per Aspera Ad Astra";
+        String plainText = "b";
+        // String plainText = "a-z";
+        // String plainText = "Per Aspera Ad Astra";
         int a = 5;
         int b = 15;
 
