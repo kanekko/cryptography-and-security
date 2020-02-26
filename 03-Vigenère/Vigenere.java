48114dd195a6ba029fae2f882070a1e6ba5cc8d4
Canek <kaan.ek@ciencias.unam.mx>
2020-02-25 19:48:42 -0600

    Working on new implementation

---
+++ 03-Vigenère/Vigenere.java
@@ -0,0 +1,99 @@
+public class Vigenere {
+
+    // Member viables (Constants)
+    static String ALPHABET_MINUS = "abcdefghijklmnñopqrstuvwxyz";
+    static String ALPHABET_MAYUS = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ";
+
+    /**
+     * E(x) = (Xi + Ki) mod L
+     */
+    private static String encrypt(String plainText, String key) {
+        String cryptogram = "";
+
+        for (int i=0, j=0, l=plainText.length(); i<l; i++, j=j%l) {
+
+            if( ALPHABET_MAYUS.contains( String.valueOf(plainText.charAt(i)) ) ){
+                int Xi = ALPHABET_MAYUS.indexOf(plainText.charAt(i));
+                int Ki = ALPHABET_MAYUS.indexOf(key.charAt(j));
+                System.out.println("Xi:"+Xi);
+                System.out.println("Ki:"+Ki);
+                // E(x)= ( Xi + Ki ) mod L
+                int Ex = ( Xi + Ki ) % ALPHABET_MAYUS.length();
+                char symbol = ALPHABET_MAYUS.charAt(Ex);
+
+                cryptogram += symbol; 
+            }else{
+                cryptogram += plainText.charAt(i); 
+            }
+
+        }
+
+        return cryptogram;
+    }
+
+    /**
+     * D(x) = (Ci + Ki) mod L
+     */
+    private static String decrypt(String cryptogram, String key) {
+        String plainText = "";
+
+        for (int i=0, j=0, l=cryptogram.length(); i<l; i++, j=j%l) {
+            if( ALPHABET_MAYUS.contains( String.valueOf(cryptogram.charAt(i)) ) ){
+
+                int Ci = ALPHABET_MAYUS.indexOf(cryptogram.charAt(i));
+                int Ki = ALPHABET_MAYUS.indexOf(key.charAt(j));
+                int Dx = 0;
+                
+                if( (Ci-Ki)>=0 ){
+                    // D(x)= ( Ci - Ki ) mod L
+                    Dx = ( Ci - Ki ) % ALPHABET_MAYUS.length();
+                }else{
+                    // D(x)= ( Ci - Ki + L ) mod L
+                    Dx = ( Ci - Ki + l ) % ALPHABET_MAYUS.length();
+                }
+
+                char cipherChar = ALPHABET_MAYUS.charAt(Dx);
+
+                plainText += cipherChar; 
+
+
+            }else{
+                plainText += cryptogram.charAt(i); 
+            }
+        }
+
+        return plainText;
+    }
+
+
+    /**
+     * Main method
+     *
+     * @param args argumentos
+     */
+    public static void main(String[] args) {
+        // String mensaje = "PER ASPERA AD ASTRA";
+        // String llave   = "VIGENERE";
+        // String plainText = "ATTACKATDAWN";
+        // String key       = "LEMON";
+        String plainText = "PARISVAUTBIENUNEMESSE";
+        String key       = "LOUP";
+
+        String cryptogram  = "";
+        String decodedText = "";
+
+        // 1. Plain Text
+		System.out.println("Texto plano:     " + plainText);
+		System.out.println("Clave:           " + key);
+		System.out.println();        
+
+		// 2. Encrypt
+        cryptogram = encrypt(plainText, key); 
+        System.out.println("Criptograma:   " + cryptogram); 
+
+        // 3. Decrypt
+        // decodedText = decrypt(cryptogram, key); 
+        // System.out.println("Texto decodificado: " + decodedText); 
+    }
+
+}
